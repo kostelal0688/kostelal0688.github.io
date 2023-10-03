e155a65b40914dd4dba387d9ceaf170912dc8d43
kostelal0688 <146861788+kostelal0688@users.noreply.github.com>
2023-10-03 11:59:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Kostela L"; // update the part BETWEEN the "quotes"
+let userName = "kostelal0688"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
